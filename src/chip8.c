3e99c1f7c3ad4e11ee58c73d872edfa19f473111
dealbertt <dealbertignacio@gmail.com>
2025-08-14 22:39:12 +0200

    Finished keypad

--- src/chip8.c
+++ src/chip8.c
@@ -394,7 +394,29 @@ int waitForPress(){
     }else if(pressed[SDL_SCANCODE_4]){
         return 0xC;
     }else if(pressed[SDL_SCANCODE_Q]){
+        return 0x4;
+    }else if(pressed[SDL_SCANCODE_W]){
         return 0x5;
+    }else if(pressed[SDL_SCANCODE_E]){
+        return 0x6;
+    }else if(pressed[SDL_SCANCODE_R]){
+        return 0xD;
+    }else if(pressed[SDL_SCANCODE_A]){
+        return 0x7;
+    }else if(pressed[SDL_SCANCODE_S]){
+        return 0x8;
+    }else if(pressed[SDL_SCANCODE_D]){
+        return 0x9;
+    }else if(pressed[SDL_SCANCODE_F]){
+        return 0xE;
+    }else if(pressed[SDL_SCANCODE_Z]){
+        return 0xA;
+    }else if(pressed[SDL_SCANCODE_X]){
+        return 0x0;
+    }else if(pressed[SDL_SCANCODE_C]){
+        return 0xB;
+    }else if(pressed[SDL_SCANCODE_V]){
+        return 0xF;
     }
     return 0;
 }
